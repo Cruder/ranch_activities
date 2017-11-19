64dc0df424287cee221f83311b65e716e7332811
alex-lairan <lairana@free.fr>
2017-11-19 10:22:15 +0100

    Add auth

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
 
   root to: 'sessions#index'
 
-  devise_for :users
+  devise_for :users, only: %i[]
+
+  resources :users do
+    collection do
+      resources :auth, controller: 'authenticates', only: %i[new create destroy]
+    end
+  end
 end
